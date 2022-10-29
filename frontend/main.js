a3cde2e891f3c1d799f430b85ec09a994b08d81f
hparrish0415 <112846254+hparrish0415@users.noreply.github.com>
2022-10-29 16:24:31 -0500

    Deployed Storage and Function to Azure

--- frontend/main.js
+++ frontend/main.js
@@ -2,11 +2,12 @@ window.addEventListener('DOMContentLoaded', (event) =>{
     getVisitCount();
 })
 
-const functionApi = 'http://localhost:7071/api/GetResumeCounter';
+const functionApiUrl = 'https://getresumecounter1.azurewebsites.net/api/GetResumeCounter?code=9mlkfWRPvtCAodNjoR3g99A4Cg-B_Ku-GfeWVAyL3IOsAzFuTHubZg=='
+const localFunctionApi = 'http://localhost:7071/api/GetResumeCounter';
 
 const getVisitCount = () => {
     let count = 30;
-    fetch(functionApi).then(response => {
+    fetch(functionApiUrl).then(response => {
         return response.json()
     }).then(response =>{
         console.log("Website called function API.")
